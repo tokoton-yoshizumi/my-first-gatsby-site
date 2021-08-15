7b00185b23a4f1dd9867ad629e2339ab2abe68c2
tokoton-yoshizumi <info@tokoton-design.com>
2021-08-16 00:10:52 +0900

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,31 +1,42 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
-const BlogPage = ({ data }) => {
-  return (
-    <Layout pageTitle="My Blog Posts">
-      <ul>
-      {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
-        ))
-      }
-      </ul>
-    </Layout>
-  )
-}
+// ...
+// imports
 
-export const query = graphql`
-  query {
-    allFile {
-      nodes {
-        name
+const BlogPage = ({ data }) => {
+    return (
+      <Layout pageTitle="My Blog Posts">
+        {
+          data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+            </article>
+          ))
+        }
+      </Layout>
+    )
+  }
+  
+  export const query = graphql`
+    query {
+      allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+        nodes {
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+          }
+          id
+          body
+        }
       }
     }
-  }
-`
-
-export default BlogPage
+  `
+  
+  export default BlogPage
